80fed8167180046478b96e852a95dfe35518ed02
Larry Anderson <larryboymi@hotmail.com>
2016-10-22 13:22:56 -0500

    removing from route

--- services/sms/notifyOfAvailability.js
+++ services/sms/notifyOfAvailability.js
@@ -1,10 +1,9 @@
 const getSNSTopic = require('./getSNSTopic')
 const snsPublish = require('./snsPublish')
 
-const subscribe = (req, res, next) =>
-  getSNSTopic(req.query.shelterId)
+const notify = (shelterId) =>
+  getSNSTopic(shelterId)
   .then((shelter) => snsPublish(shelter['sns-topic'], undefined,
     `You requested to receive notifications of available beds at ${shelter['name']}. One may be available. Please contact ${shelter['contact']['phone']} for info.`))
-  .then((resp) => res.json(resp))
 
-module.exports = subscribe
+module.exports = notify
