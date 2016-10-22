df7095c18b1f27a096e011e3530b35232fcafdff
Bryan Young <Bryan.Young@monsanto.com>
2016-10-21 23:17:10 -0500

    mocked in bed search results

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, browserHistory } from 'react-router'
 import App from './App'
-import BedFinder from './bed-finder'
+import {BedFinder, Results} from './bed-finder'
 
 const routes =
   <Router history={browserHistory}>
     <Route path="/" component={App}/>
     <Route path="/bed" component={BedFinder}/>
+    <Route path="/bed/results" component={Results}/>
   </Router>
 
 render(routes, document.getElementById('main'))
