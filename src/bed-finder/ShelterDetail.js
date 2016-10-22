a395c158d98ad3252e1853c7b510972c313dfa3c
Bryan Young <Bryan.Young@monsanto.com>
2016-10-22 12:34:00 -0500

    prompting user for phone number if they request notification of an empty bed

--- src/bed-finder/ShelterDetail.js
+++ src/bed-finder/ShelterDetail.js
@@ -1,27 +1,35 @@
 import React from "react";
 import {render} from "react-dom";
 
-const ShelterDetail = () =>
-    <div className="shelter-detail">
-        <div className="title">ShelterName</div>
-        <div className="contact-info">
-            <div>123 Birch St</div>
-            <div>(555) 456-2989</div>
-        </div>
-        <div className="map">map goes here</div>
-        <ul className="detail">
-            <li>1.2 miles</li>
-            <li>16 min walk</li>
-            <li>5 min public transit</li>
-            <li>0 open beds - <a href="">notify me</a></li>
-        </ul>
-        <div className="restrictions">
-            <div>Restrictions</div>
-            <ul>
-                <li>men only</li>
-                <li>no Trump supporters</li>
+const ShelterDetail = ({shelter, requestNotification}) => {
+    function notify(e) {
+        e.preventDefault()
+        requestNotification(shelter.id)
+    }
+
+    return (
+        <div className="shelter-detail">
+            <div className="title">ShelterName</div>
+            <div className="contact-info">
+                <div>123 Birch St</div>
+                <div>(555) 456-2989</div>
+            </div>
+            <div className="map">map goes here</div>
+            <ul className="detail">
+                <li>1.2 miles</li>
+                <li>16 min walk</li>
+                <li>5 min public transit</li>
+                <li>0 open beds - <a href="" onClick={notify}>notify me</a></li>
             </ul>
+            <div className="restrictions">
+                <div>Restrictions</div>
+                <ul>
+                    <li>men only</li>
+                    <li>no Trump supporters</li>
+                </ul>
+            </div>
         </div>
-    </div>
+    )
+}
 
 export default ShelterDetail
