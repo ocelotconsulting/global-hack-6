ed3d7ec3f9269a08eb31a50227c43603792808c4
John O'Malley <john.omalley@ocelotconsulting.com>
2016-10-22 17:07:56 -0500

    check in

--- src/admin/CheckInClient.js
+++ src/admin/CheckInClient.js
@@ -2,6 +2,10 @@
 
 import React from 'react'
 import agent from '../agent'
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar'
+import ButtonGroup from 'react-bootstrap/lib/ButtonGroup'
+import Button from 'react-bootstrap/lib/Button'
+import { Link } from 'react-router'
 
 export default class Checkin extends React.Component {
   updateClient(clientId = this.props.params.clientId) {
@@ -20,10 +24,27 @@ export default class Checkin extends React.Component {
   render() {
     const { client } = this.state || {}
 
-    return (
-      <div className='check-in'>
-        {client && client.name}
-      </div>
-    )
+    if (client) {
+      return (
+        <div className='check-in'>
+          <h2>
+            {`${client.first_name} ${client.last_name}`}
+          </h2>
+          <hr/>
+          <ButtonToolbar>
+            <ButtonGroup>
+              <Button bsStyle='primary'>Check In</Button>
+            </ButtonGroup>
+            <ButtonGroup>
+              <Link to={`/admin/${encodeURIComponent(this.props.params.shelterId)}`} className='btn btn-default'>
+                Cancel
+              </Link>
+            </ButtonGroup>
+          </ButtonToolbar>
+        </div>
+      )
+    } else {
+      return (<div className='spinner'/>)
+    }
   }
 }
