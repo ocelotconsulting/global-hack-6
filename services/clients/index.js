3ddb53c8528e2c219a81bca5db467cc8eb80dd05
Larry Anderson <larryboymi@hotmail.com>
2016-10-22 13:29:43 -0500

    separating

--- services/clients/index.js
+++ services/clients/index.js
@@ -1,22 +1,7 @@
 const express = require('express')
-const _ = require('underscore')
-const mango = require('../mango')
 const router = express.Router()
+const searchUser = require('./searchUser')
 
-const clientMango = (q) => ({
-  selector: {
-    _id: {
-      $gt: null
-    },
-    last_name: {
-      $regex: `(?i)^(${q})`
-    }
-  }
-})
-
-router.get('/', (req, res) =>
-  mango('clients', clientMango(req.query.q))
-  .then((clients) => res.json(_(clients).sortBy('first_name')))
-)
+router.get('/', searchUser)
 
 module.exports = router
