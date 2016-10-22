a3aa79e8f5196799f097c677bd5752f0e026ea34
John O'Malley <john.omalley@ocelotconsulting.com>
2016-10-22 16:33:46 -0500

    use same view for single-item get as multi

--- services/shelters/getAll.js
+++ services/shelters/getAll.js
@@ -1,8 +1,9 @@
 const agent = require('../../src/agent')
 const { couchUrl } = require('../config')
 
-module.exports = () =>
+module.exports = (query = {}) =>
   agent.get(`${couchUrl}/shelters/_design/beds/_view/all_with_summary`)
+  .query(query)
   .then(({ body: { rows } }) =>
     rows.map(row => {
       row.value.id = row.id
