e07119bf763349de659241d36c2c828e4450ff22
PMByrne <mudrummer012@gmail.com>
2016-10-22 14:17:41 -0500

    Added a favicon via Bryan

--- page.js
+++ page.js
@@ -7,6 +7,7 @@ module.exports = `
   <meta charset="UTF-8">
   <meta name = "viewport" content = "initial-scale = 1.0, user-scalable = no">
   <title>${title}</title>
+  <link href="/favicon.ico" rel="shortcut icon">
   <link rel="stylesheet" type="text/css" href="/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="/chartist/dist/chartist.min.css">
   <link rel="stylesheet" type="text/css" href="/styles.css">
