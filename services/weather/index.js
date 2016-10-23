92f7ff5060d1168939172bbec24b7bfbcb02d8f4
dbtand <dbtand@monsanto.com>
2016-10-23 08:03:49 -0500

    added weather history couch data

--- services/weather/index.js
+++ services/weather/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const getForecast = require('./getForecast')
+const getHistory = require('./history')
 
 router.get('/', (req, res, next) =>
   getForecast()
   .then(forecast => res.json(forecast))
+  .catch(next)
+)
+
+router.get('/history', (req, res, next) =>
+  getHistory()
+  .then(history => res.json(history))
   .catch(next)
 )
 
