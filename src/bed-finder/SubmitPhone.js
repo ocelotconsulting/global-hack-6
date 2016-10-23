446dca007cbcf51bfa0c6661bd766f2a0fd2ddfc
Larry Anderson <larryboymi@hotmail.com>
2016-10-22 20:43:02 -0500

    help with intl code

--- src/bed-finder/SubmitPhone.js
+++ src/bed-finder/SubmitPhone.js
@@ -5,7 +5,8 @@ import {render} from "react-dom";
 export default class ProfileDetails extends React.Component {
   submit(e) {
     e.preventDefault()
-    this.props.requestNotification(this.props.shelter.id, this.state.number)
+    const num = this.state.number.replace(/\D/g, '')
+    this.props.requestNotification(this.props.shelter.id, (num.length === 11) ? num : ('11111111111' + num).slice(-11))
   }
   handleChange(e) {
     this.setState({number: e.target.value})
@@ -17,7 +18,7 @@ export default class ProfileDetails extends React.Component {
             <form>
                 <div className="form-group">
                     <label>Number</label>
-                    <input className="form-control" type="tel" placeholder="optional" onChange={(e) => this.handleChange(e)}/>
+                    <input className="form-control" type="tel" placeholder="3145555555" onChange={(e) => this.handleChange(e)}/>
                 </div>
                 <button className="btn btn-primary" onClick={(e) => this.submit(e)}>Confirm</button>
                 <div className="alert alert-warning" role="alert">A single text message is sent when a bed is free.
