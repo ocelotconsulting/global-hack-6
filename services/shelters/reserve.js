82be9984d26f358900c310843377199622508e59
dbtand <dbtand@monsanto.com>
2016-10-22 20:30:12 -0500

    Added basic validation to reservation for requesting more beds then are available

--- services/shelters/reserve.js
+++ services/shelters/reserve.js
@@ -1,13 +1,28 @@
 const getDoc = require('../clients/getDoc')
+const getById = require('./getById')
 const putDoc = require('../clients/putDoc')
 const db = 'shelters'
 
 module.exports = ({ shelterId, clientId, bedTypes }) => {
-  return getDoc(db, shelterId)
+  return getById(shelterId)
+  .then(shelterSummary => {
+    const totalBedsAvailable = shelterSummary.beds
+    .map((bed) => bed.total_beds - bed.total_taken)
+    .reduce(((currValue, nextValue) => currValue + nextValue), 1)
+
+    let bedRequestCount = 0
+    for (bedType in bedTypes) {
+      bedRequestCount += bedTypes[bedType]
+    }
+    if (bedRequestCount > totalBedsAvailable)
+      throw `Requested ${bedRequestCount} when there are only ${totalBedsAvailable}.`
+  })
+  .then(() => getDoc(db, shelterId))
   .then(shelterDoc => {
-    console.log('getDoc shelter: ', shelterDoc)
     for (bedType in bedTypes) {
       if (bedTypes[bedType] > 0) {
+        if (!shelterDoc.reservations)
+          shelterDoc.reservations = {}
         if (!shelterDoc.reservations[bedType])
           shelterDoc.reservations[bedType] = []
         shelterDoc.reservations[bedType].push({clientId: clientId, created: new Date()})
