2dff0f60056fff25df34cfa9e04b4da93c8d0fe7
Larry Anderson <larryboymi@hotmail.com>
2016-10-23 08:29:49 -0500

    added notification

--- services/shelters/clearReservation.js
+++ services/shelters/clearReservation.js
@@ -3,7 +3,7 @@ const getById = require('./getById')
 const putDoc = require('../clients/putDoc')
 const db = 'shelters'
 const sendEmail = require('./sendEmail')
-
+const notify = require('../sms/notifyOfAvailability')
 module.exports = ({ shelterId, clientName, checkedIn }) => {
   console.log('user checked in? or was the reservation canceled?', checkedIn)
   // todo email the person who made the reservation (found in shelterDoc.reservations2.requestor.email (and .name)
@@ -13,8 +13,9 @@ module.exports = ({ shelterId, clientName, checkedIn }) => {
     if (existingReservation && existingReservation.requestor && existingReservation.requestor.email) {
       sendEmail(existingReservation)
     }
-    shelterDoc.reservations2 = shelterDoc.reservations2.filter((reservation) => reservation.clientName != clientName)
+    shelterDoc.reservations2 = shelterDoc.reservations2.filter((reservation) => reservation.clientName !== clientName)
     return putDoc(db, shelterDoc)
+      .then((res) => notify(shelterId))
   })
   .then(() => getById(shelterId))
   // todo free up the beds that were reserved
